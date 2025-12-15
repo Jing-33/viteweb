a82103595bbdadcc2d8dd4bf445f87d66ba970a5
Jing-33 <1113211037@gap.wzu.edu.tw>
2025-12-15 12:01:38 +0800

    123

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+// src/main.js
+
+// 確保匯入了 Bootstrap 的 JavaScript (包含 Popper.js)
+import * as bootstrap from 'bootstrap';
+
+// 確保 CSS/SCSS 入口檔案被引用
+import './scss/main.scss';
+
+console.log('文藻小咖啡 網頁腳本載入完成！');
